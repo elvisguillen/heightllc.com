1c66f08e64c833a0f5c917324c14287bc96d1a70
Elvis Guillen <elvisguillen@Elviss-MacBook-Pro.local>
2018-02-05 12:24:36 -0500

    Updated JS Caching on Gatsby Node

--- gatsby-node.js
+++ gatsby-node.js
@@ -116,3 +116,18 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
   })
 }
 
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  const timestamp = Date.now()
+  switch (stage) {
+    case 'build-javascript':
+      config.merge({
+        output: {
+          filename: `[name]-${timestamp}-[chunkhash].js`,
+          chunkFilename: `[name]-${timestamp}-[chunkhash].js`
+        }
+      })
+
+      break
+  }
+  return config
+}
